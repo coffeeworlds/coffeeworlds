09516c83c0a0d94f4c16e1b48fcd7e74af0cd369
ChillerDragon <chillerdragon@gmail.com>
2024-11-01 11:07:44 +0800

    Add packet unpack test

--- network/src/test/java/com/github/coffeeworlds/network/PacketTest.java
+++ network/src/test/java/com/github/coffeeworlds/network/PacketTest.java
@@ -3,11 +3,12 @@ package com.github.coffeeworlds.network;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.github.coffeeworlds.network.system.MsgInfo;
+import java.util.HexFormat;
 import org.junit.jupiter.api.Test;
 
 class PacketTest {
   @Test
-  void basic() {
+  void pack() {
     PacketHeader header = new PacketHeaderBuilder().buildHeader();
     Packet packet = new Packet(header);
 
@@ -24,5 +25,22 @@ class PacketTest {
           48, 97, 48, 53, 54, 54, 53, 102, 0, 0, -123, 28
         };
     assertArrayEquals(expected, data);
+  }
+
+  @Test
+  void unpack() {
+    byte[] data =
+        HexFormat.ofDelimiter(" ")
+            .parseHex(
+                // packet header
+                "00 00 01 26 3e 5a 37 "
+                    + "40 28 01 03 30 2e 37 20 38 30 32 66 31 62 65 36 30 61 "
+                    + "30 35 36 36 35 66 00 6d 79 5f 70 61 73 73 77 6f 72 64 "
+                    + "5f 31 32 33 00 85 1c");
+    Packet packet = new Packet(data);
+    assertEquals(false, packet.header.flags.control);
+    assertEquals(false, packet.header.flags.compression);
+    assertEquals(false, packet.header.flags.resend);
+    assertEquals(false, packet.header.flags.connless);
   }
 }
